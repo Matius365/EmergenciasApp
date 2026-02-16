702ede0705b31bd3e31a2ca65cf49b51c6659f89
Matius365 <macius365@gmail.com>
2026-02-16 11:04:33 +0100

    cambios para guardar en json

--- src/model/EmergencyEvent.java
+++ src/model/EmergencyEvent.java
@@ -9,6 +9,8 @@ public class EmergencyEvent {
     private String datosUsuario;
     private String fechaHora; // nueva feature agrega la fecha y la hora de la emergencia
 
+    public EmergencyEvent(){} //constructor vacio para JSON
+
     public EmergencyEvent(String tipoEmergencia, String ubicacion, String datosUsuario) {
         this.tipoEmergencia = tipoEmergencia;
         this.ubicacion = ubicacion;
@@ -26,6 +28,26 @@ public class EmergencyEvent {
     public String getDatosUsuario() {
         return datosUsuario;
     }
+    public String getFechaHora() {
+        return fechaHora;
+    }
+
+    public void setTipoEmergencia(String tipoEmergencia) {
+        this.tipoEmergencia = tipoEmergencia;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public void setDatosUsuario(String datosUsuario) {
+        this.datosUsuario = datosUsuario;
+    }
+
+    public void setFechaHora(String fechaHora) {
+        this.fechaHora = fechaHora;
+    }
+
     @Override
     public String toString() {
         return "Tipo: " + tipoEmergencia + ", Ubicación: " + ubicacion + ", Usuario: " + datosUsuario
