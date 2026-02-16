6ac38880b3e5009ad6f090f3e00b05b40cfbe2a3
Matius365 <macius365@gmail.com>
2026-02-16 11:47:52 +0100

    cambios para guardar en json

--- src/alert/AlertSender.java
+++ src/alert/AlertSender.java
@@ -1,8 +1,13 @@
 package alert;
 
 import model.EmergencyEvent;
-import java.io.FileWriter;
-import java.io.IOException;
+import tools.jackson.core.type.TypeReference;
+import tools.jackson.databind.ObjectMapper;
+import java.io.File;
+//import java.io.FileWriter;
+//import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AlertSender {
     private String destino;
@@ -19,11 +24,35 @@ public class AlertSender {
 
         System.out.println("Enviando alerta a " + destino + ": " + event);
 
-        // Persistencia en archivo
-        try (FileWriter writer = new FileWriter("src/resources/alertas.txt", true)) {
-            writer.write(event.toString() + "\n");
-        } catch (IOException e) {
-            System.err.println("Error al guardar alerta: " + e.getMessage());
+        // Creamos archivo .txt para guardar alertas
+//        try (FileWriter writer = new FileWriter("src/resources/alertas.txt", true)) {
+//            writer.write(event.toString() + "\n");
+//        } catch (IOException e) {
+//            System.err.println("Error al guardar alerta: " + e.getMessage());
+//        }
+
+        // Creamos archivo .json para guardar alertas
+        ObjectMapper mapper = new ObjectMapper();
+        File file = new File("src/resources/alertas.json");
+
+        try {
+            List<EmergencyEvent> lista;
+
+            if (file.exists()){
+                lista = mapper.readValue(file,
+                        new TypeReference<List<EmergencyEvent>>() {});
+            } else {
+                lista = new ArrayList<>();
+            }
+            //Añadimos la alerta a lista
+            lista.add(event);
+
+            //Guardamos el JSON actualizado
+            mapper.writerWithDefaultPrettyPrinter()
+                    .writeValue(file, lista);
+
+        } catch (Exception e){
+            System.err.println("Error al guardar alertas.json: " + e.getMessage());
         }
     }
 
