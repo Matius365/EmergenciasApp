36feabab2478bdf018cd22698bc08c9817d38ebc
Matius365 <macius365@gmail.com>
2026-02-16 12:16:54 +0100

    cambios para guardar y leer en json

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,8 +5,13 @@ import alert.AlertSender;
 import controller.EmergencyManager;
 import model.CentroSalud;
 import model.CentroSaludService;
-import java.io.BufferedReader;
-import java.io.FileReader;
+import model.EmergencyEvent;
+import tools.jackson.core.type.TypeReference;
+import tools.jackson.databind.ObjectMapper;
+
+//import java.io.BufferedReader;
+import java.io.File;
+//import java.io.FileReader;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
@@ -75,20 +80,50 @@ public class Main {
         }
     }
 
+
+    //Este es el metodo para leer el historico de alertas.txt
+//    public static void historicoAlertas(){
+//        try (BufferedReader br =
+//                new BufferedReader(new FileReader("src/resources/alertas.txt"))){
+//
+//            String linea;
+//            System.out.println("===HISTORICO DE ALERTAS===");
+//
+//            while ((linea = br.readLine()) != null){
+//                System.out.println(linea);
+//            }
+//
+//        } catch (java.io.IOException e) {
+//            System.out.println("Error al leer el archivo " + e.getMessage());
+//        }
+//
+//    }
+
+    //Metodo para leer alertas.json
     public static void historicoAlertas(){
-        try (BufferedReader br =
-                new BufferedReader(new FileReader("src/resources/alertas.txt"))){
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            File file = new File("src/resources/alertas.json");
 
-            String linea;
-            System.out.println("===HISTORICO DE ALERTAS===");
+            if(!file.exists()){
+                System.out.println("No hay alertas registradas.");
+                return;
+            }
+            List<EmergencyEvent> lista =
+                    mapper.readValue(file,
+                            new TypeReference<List<EmergencyEvent>>() {});
 
-            while ((linea = br.readLine()) != null){
-                System.out.println(linea);
+            System.out.println("====HISTORICO DE ALERTAS====");
+
+            for (EmergencyEvent e : lista){
+                System.out.println(e.getTipoEmergencia() + " - "
+                + e.getUbicacion() + " - "
+                + e.getDatosUsuario() +" - "
+                + e.getFechaHora());
             }
 
-        } catch (java.io.IOException e) {
-            System.out.println("Error al leer el archivo " + e.getMessage());
+        } catch (Exception e){
+            System.out.println("Error leyendo alertas.json: " + e.getMessage());
         }
-
     }
 }
