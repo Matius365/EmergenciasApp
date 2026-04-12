5f35d9fa48a08590de92cfe92fbdbcc6178705ba
Matius365 <macius365@gmail.com>
2026-04-12 19:10:26 +0200

    base de datos en JavaFX

--- src/database/DBConnection.java
+++ src/database/DBConnection.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/Agenda";
     private static final String USER = "root";
-    private static final String PASSWORD = ""; // tu contraseña
+    private static final String PASSWORD = ""; //contraseña
 
     public static Connection getConnection() throws Exception {
         return DriverManager.getConnection(URL, USER, PASSWORD);
